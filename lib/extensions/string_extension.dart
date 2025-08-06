660c57e4c51e90eebfaca620bcaf83bd886f5050
David Benitez <dbr.bcn@gmail.com>
2025-08-06 05:32:08 +0200

    add capitalizacion extension

---
+++ lib/extensions/string_extension.dart
@@ -0,0 +1,12 @@
+extension StringCasingExtension on String {
+  String capitalize() {
+    if (isEmpty) return this;
+    return this[0].toUpperCase() + substring(1);
+  }
+
+  String capitalizeEachWord() {
+    return split(' ')
+        .map((word) => word.capitalize())
+        .join(' ');
+  }
+}
