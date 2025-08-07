c76e89a4c2d5c8e44f28dc42a5f680b728d01b66
David Benitez <dbr.bcn@gmail.com>
2025-08-07 13:02:36 +0200

    adds currencies

--- lib/domain/currency.dart
+++ lib/domain/currency.dart
@@ -1,7 +1,1 @@
-class Currency {
-  final String code;
-  final String name;
-  final String symbol;
-
-  const Currency(this.code, this.name, this.symbol);
-}
+enum Currency { USD, EUR, GBP, JPY, CAD, AUD }
