5acbed9492746ceeedff7611f90de1790e2d3f02
David Benitez <dbr.bcn@gmail.com>
2025-08-08 16:50:33 +0200

    updates methods usages

--- lib/presentation/widgets/category_dropdown_menu_item.dart
+++ lib/presentation/widgets/category_dropdown_menu_item.dart
@@ -1,5 +1,6 @@
 import 'package:dukoin/domain/expense.dart';
 import 'package:dukoin/l10n/app_localizations.dart';
+import 'package:dukoin/utils/utils.dart';
 import 'package:flutter/material.dart';
 
 class CategoryDropdownMenuItem extends StatelessWidget {
@@ -7,56 +8,14 @@ class CategoryDropdownMenuItem extends StatelessWidget {
 
   const CategoryDropdownMenuItem({super.key, required this.category});
 
-  String getIcon() {
-    switch (category) {
-      case ExpenseCategory.food:
-        return '🍽️';
-      case ExpenseCategory.transport:
-        return '🚗';
-      case ExpenseCategory.shopping:
-        return '🛍️';
-      case ExpenseCategory.entertainment:
-        return '🎬';
-      case ExpenseCategory.bills:
-        return '📄';
-      case ExpenseCategory.health:
-        return '🏥';
-      case ExpenseCategory.education:
-        return '📚';
-      default:
-        return '💰';
-    }
-  }
-
-  String getTitle(BuildContext context) {
-    switch (category) {
-      case ExpenseCategory.food:
-        return AppLocalizations.of(context)!.categoryFood;
-      case ExpenseCategory.transport:
-        return AppLocalizations.of(context)!.categoryTransport;
-      case ExpenseCategory.shopping:
-        return AppLocalizations.of(context)!.categoryShopping;
-      case ExpenseCategory.entertainment:
-        return AppLocalizations.of(context)!.categoryEntertainment;
-      case ExpenseCategory.bills:
-        return AppLocalizations.of(context)!.categoryBills;
-      case ExpenseCategory.health:
-        return AppLocalizations.of(context)!.categoryHealth;
-      case ExpenseCategory.education:
-        return AppLocalizations.of(context)!.categoryEducation;
-      default:
-        return AppLocalizations.of(context)!.categoryOthers;
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(getIcon()),
+        Text(getIconFromCategory(category)),
         Padding(
           padding: const EdgeInsets.only(left: 8.0),
-          child: Text(getTitle(context)),
+          child: Text(getCategoryTitle(context, category)),
         ),
       ],
     );
