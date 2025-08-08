2e02601b829062a976ec75e1f102f1bf6b9830e4
David Benitez <dbr.bcn@gmail.com>
2025-08-08 14:33:07 +0200

    adds clear all data dialog

---
+++ lib/presentation/widgets/clear_all_data_dialog.dart
@@ -0,0 +1,78 @@
+import 'package:dukoin/l10n/app_localizations.dart';
+import 'package:dukoin/presentation/state/expense_provider.dart';
+import 'package:dukoin/styles/dukoin_colors.dart';
+import 'package:flutter/material.dart';
+
+class ClearAllDataDialog extends StatelessWidget {
+  const ClearAllDataDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.clearAllDataDialogTitle,
+                style: Theme.of(context).textTheme.displayMedium,
+                textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 12.0, top: 8),
+                child: Text(
+                  AppLocalizations.of(context)!.clearAllDataDialogMessage,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    ExpensesProvider.of(context).clearAllData();
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).colorScheme.error,
+                  ),
+                  child: Text(
+                    AppLocalizations.of(context)!.clearAllDataDialogButtonTitle,
+                  ),
+                ),
+              ),
+              //SizedBox(height: 2.0),
+              SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(
+                      color: Theme.of(
+                        context,
+                      ).extension<DukoinColors>()!.borderColor,
+                      width: 1,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text(
+                    AppLocalizations.of(
+                      context,
+                    )!.clearAllDataDialogCancelButton,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
