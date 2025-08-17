256e73a5a7b7e8ab2a969671fab2acaf58a22b49
David Benitez <dbr.bcn@gmail.com>
2025-08-18 00:50:38 +0200

    adds refreshing indicator

--- lib/presentation/widgets/history_view.dart
+++ lib/presentation/widgets/history_view.dart
@@ -1,15 +1,22 @@
 import 'package:dukoin/domain/expense.dart';
 import 'package:dukoin/l10n/app_localizations.dart';
+import 'package:dukoin/presentation/state/expense_provider.dart';
+import 'package:dukoin/presentation/state/expenses_bloc.dart';
 import 'package:dukoin/presentation/widgets/dismissible_expense_info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:intl/intl.dart';
 
-class ExpenseHistoryListView extends StatelessWidget {
-  final List<Expense> expenses;
+class ExpenseHistoryListView extends StatefulWidget {
+  List<Expense> expenses;
 
-  const ExpenseHistoryListView({super.key, required this.expenses});
+  ExpenseHistoryListView({super.key, required this.expenses});
 
+  @override
+  State<ExpenseHistoryListView> createState() => _ExpenseHistoryListViewState();
+}
+
+class _ExpenseHistoryListViewState extends State<ExpenseHistoryListView> {
   String formatDateTitle(DateTime date, AppLocalizations appLocales) {
     final now = DateTime.now();
     final today = DateTime(now.year, now.month, now.day);
@@ -27,10 +34,17 @@ class ExpenseHistoryListView extends StatelessWidget {
     }
   }
 
+  Future<void> _refresh(ExpensesBloc bloc) async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      //widget.expenses = bloc.expenses;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final Map<DateTime, List<Expense>> grouped = {};
-    for (final e in expenses) {
+    for (final e in widget.expenses) {
       final day = DateTime(
         e.createdAt.year,
         e.createdAt.month,
@@ -39,39 +53,50 @@ class ExpenseHistoryListView extends StatelessWidget {
       grouped.putIfAbsent(day, () => []).add(e);
     }
 
-    return SafeArea(
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(title: Text(AppLocalizations.of(context)!.historyTitle)),
-          ...grouped.entries.map((entry) {
-            final date = entry.key;
-            final expenses = entry.value;
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) => [
+        SliverAppBar(
+          title: Text(AppLocalizations.of(context)!.historyTitle),
+          pinned: true, // keeps the app bar visible
+          flexibleSpace: FlexibleSpaceBar(
+            //background: Container(color: Theme.of(context).primaryColor),
+          ),
+        ),
+      ],
+      body: RefreshIndicator(
+        onRefresh: () => _refresh(ExpensesProvider.of(context)),
+        child: CustomScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          slivers: [
+            ...grouped.entries.map((entry) {
+              final date = entry.key;
+              final expenses = entry.value;
 
-            return SliverPadding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              sliver: SliverStickyHeader(
-                header: Container(
-                  color: Theme.of(context).colorScheme.surface,
-                  padding: const EdgeInsets.all(16.0),
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    formatDateTitle(date, AppLocalizations.of(context)!),
-                    style: TextTheme.of(context).displaySmall,
+              return SliverPadding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                sliver: SliverStickyHeader(
+                  header: Container(
+                    color: Theme.of(context).colorScheme.surface,
+                    padding: const EdgeInsets.all(16.0),
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      formatDateTitle(date, AppLocalizations.of(context)!),
+                      style: TextTheme.of(context).displaySmall,
+                    ),
+                  ),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (context, i) =>
+                          DismissibleExpenseInfoCard(expense: expenses[i]),
+                      childCount: expenses.length,
+                    ),
                   ),
                 ),
-                sliver: SliverList(
-                  delegate: SliverChildBuilderDelegate((context, i) {
-                    final expense = expenses[i];
-                    return DismissibleExpenseInfoCard(expense: expense);
-                  }, childCount: expenses.length),
-                ),
-              ),
-            );
-          }),
-          SliverToBoxAdapter(
-            child: SizedBox(height: 32), // adjust height as needed
-          ),
-        ],
+              );
+            }),
+            SliverToBoxAdapter(child: SizedBox(height: 32)),
+          ],
+        ),
       ),
     );
   }
