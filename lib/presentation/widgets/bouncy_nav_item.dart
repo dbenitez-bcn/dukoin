25b9a18a73c7d10663cf72e72397bbe612ea5cee
David Benitez <dbr.bcn@gmail.com>
2025-08-21 13:26:25 +0200

    improves animation

--- lib/presentation/widgets/bouncy_nav_item.dart
+++ lib/presentation/widgets/bouncy_nav_item.dart
@@ -20,33 +20,48 @@ class BouncyNavItem extends StatefulWidget {
 }
 
 class _BouncyNavItemState extends State<BouncyNavItem>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
+    with TickerProviderStateMixin {
+  late AnimationController _tapController; // for bounce
+  late AnimationController _pressController; // for press down
+  late Animation<double> _tapScale;
+  late Animation<double> _pressScale;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
+    // Bounce animation (1.0 -> 1.2 -> 1.0)
+    _tapController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 300),
     );
+    _tapScale = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 1.0, end: 1.15), weight: 50),
+      TweenSequenceItem(tween: Tween(begin: 1.15, end: 1.0), weight: 50),
+    ]).animate(CurvedAnimation(parent: _tapController, curve: Curves.easeOut));
 
-    _scaleAnimation = TweenSequence<double>([
-      TweenSequenceItem(tween: Tween(begin: 1.0, end: 1.2), weight: 50),
-      TweenSequenceItem(tween: Tween(begin: 1.2, end: 1.0), weight: 50),
-    ]).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+    // Press down animation (1.0 -> 0.9)
+    _pressController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 150),
+    );
+    _pressScale = Tween<double>(
+      begin: 1.0,
+      end: 0.9,
+    ).animate(CurvedAnimation(parent: _pressController, curve: Curves.easeOut));
   }
 
   void _handleTap() {
-    _controller.forward(from: 0);
+    // release press down before bounce
+    _pressController.reverse();
+    _tapController.forward(from: 0);
     widget.onTap();
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    _tapController.dispose();
+    _pressController.dispose();
     super.dispose();
   }
 
@@ -64,11 +79,20 @@ class _BouncyNavItemState extends State<BouncyNavItem>
 
     return GestureDetector(
       onTap: _handleTap,
+      onTapDown: (_) => _pressController.forward(),
+      // scale down
+      onTapCancel: () => _pressController.reverse(),
+      // reset if cancelled
+      onTapUp: (_) => _pressController.reverse(),
+      // reset when finger up
       child: AnimatedBuilder(
-        animation: _scaleAnimation,
+        animation: Listenable.merge([_tapController, _pressController]),
         builder: (context, child) {
+          // multiply both scales together
+          final scale = _tapScale.value * _pressScale.value;
+
           return Transform.scale(
-            scale: _scaleAnimation.value,
+            scale: scale,
             child: StreamBuilder(
               stream: NavigationStateProvider.of(context).currentPageStream,
               initialData: NavigationStateProvider.of(context).currentPageIndex,
