9315d401d6fd0b84312f0368b45cb21f70d28c72
David Benitez <dbr.bcn@gmail.com>
2025-08-24 14:17:48 +0200

    implements month dropdown menu

--- lib/presentation/widgets/statistics_app_bar.dart
+++ lib/presentation/widgets/statistics_app_bar.dart
@@ -3,6 +3,7 @@ import 'package:dukoin/l10n/app_localizations.dart';
 import 'package:dukoin/presentation/widgets/dukoin_app_bar.dart';
 import 'package:dukoin/presentation/widgets/dukoin_dropdown_menu.dart';
 import 'package:dukoin/presentation/widgets/funnel_button.dart';
+import 'package:dukoin/presentation/widgets/month_dropdown_menu.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
@@ -12,13 +13,6 @@ class StatisticsAppBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> months = List.generate(
-      12,
-      (i) => DateFormat(
-        'yMMMM',
-        Localizations.localeOf(context).languageCode,
-      ).format(DateTime(DateTime.now().year, i + 1)).capitalize(),
-    );
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -29,15 +23,7 @@ class StatisticsAppBar extends StatelessWidget {
         ),
         Row(
           children: [
-            Expanded(
-              child: DukoinDropdownMenu(
-                items: months,
-                initialValue: 0,
-                onSelected: (value) {
-                  print('Selected: $value');
-                },
-              ),
-            ),
+            Expanded(child: MonthDropdownMenu()),
             SizedBox(width: 8),
             FunnelButton(),
           ],
