9737c26ab3500cb72b073f63ccf73481b9a02cc8
David Benitez <dbr.bcn@gmail.com>
2025-08-24 13:48:15 +0200

    implements get oldest expense date

--- lib/presentation/state/stats_page_state.dart
+++ lib/presentation/state/stats_page_state.dart
@@ -1,10 +1,13 @@
 import 'package:dukoin/domain/expense.dart';
+import 'package:dukoin/domain/expense_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class StatsProvider extends InheritedWidget {
   final StatsBloc bloc;
 
-  StatsProvider({super.key, required super.child}) : bloc = StatsBloc();
+  StatsProvider({super.key, required super.child})
+    : bloc = StatsBloc(GetIt.I<ExpenseRepository>());
 
   static StatsBloc of(BuildContext context) {
     return context.dependOnInheritedWidgetOfExactType<StatsProvider>()!.bloc;
@@ -15,15 +18,21 @@ class StatsProvider extends InheritedWidget {
 }
 
 class StatsBloc {
+  final ExpenseRepository _expenseRepository;
   DateTime _selectedDate;
   List<ExpenseCategory> _selectedCategories;
+  List<DateTime> _availableMonths;
 
-  StatsBloc()
+  StatsBloc(this._expenseRepository)
     : _selectedDate = DateTime(DateTime.now().year, DateTime.now().month, 1),
-      _selectedCategories = [];
+      _selectedCategories = [],
+      _availableMonths = [
+        DateTime(DateTime.now().year, DateTime.now().month, 1),
+      ];
 
   DateTime get selectedDate => _selectedDate;
   List<ExpenseCategory> get selectedCategories => _selectedCategories;
+  List<DateTime> get availableMonths => _availableMonths;
 
   void onDateSelected(DateTime newDate) {
     _selectedDate = newDate;
@@ -31,5 +40,26 @@ class StatsBloc {
 
   void onCategoriesUpdated(List<ExpenseCategory> newCategories) {
     _selectedCategories = newCategories;
+  }
+
+  Future<void> loadAvailableMonths() async {
+    final oldestDate = await _expenseRepository.getOldestExpenseDate();
+    if (oldestDate == null) {
+      _availableMonths = [
+        DateTime(DateTime.now().year, DateTime.now().month, 1),
+      ];
+      return;
+    }
+
+    final months = <DateTime>[];
+    var current = DateTime(DateTime.now().year, DateTime.now().month, 1);
+    final oldest = DateTime(oldestDate.year, oldestDate.month, 1);
+
+    while (current.isAfter(oldest) || current.isAtSameMomentAs(oldest)) {
+      months.add(current);
+      current = DateTime(current.year, current.month - 1, 1);
+    }
+
+    _availableMonths = months;
   }
 }
--- test/presentation/state/stats_page_state_test.dart
+++ test/presentation/state/stats_page_state_test.dart
@@ -1,45 +1,90 @@
 import 'package:dukoin/domain/expense.dart';
+import 'package:dukoin/domain/expense_repository.dart';
 import 'package:dukoin/presentation/state/stats_page_state.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'expenses_bloc_test.mocks.dart';
+
+@GenerateMocks([ExpenseRepository])
 void main() {
   group("Stats page state", () {
+    final mockrepo = MockExpenseRepository();
     test("Current month should be initial date value", () {
       final now = DateTime.now();
       final initialDate = DateTime(now.year, now.month, 1);
 
-      final sut = StatsBloc();
+      final sut = StatsBloc(mockrepo);
 
       expect(sut.selectedDate, initialDate);
     });
     test("Given a new date then it should update the state", () {
       final newDate = DateTime(2023, 1, 1);
-      final sut = StatsBloc();
+      final sut = StatsBloc(mockrepo);
 
       sut.onDateSelected(newDate);
 
       expect(sut.selectedDate, newDate);
     });
 
-    test("Should load all the months that have data", () {
-      // TODO: implement
-      //throw UnimplementedError();
+    group("loadAvailableMonths", () {
+      var now = DateTime.now();
+      final currentMonth = DateTime(now.year, now.month, 1);
+      final lastMonth = DateTime(now.year, now.month - 1, 1);
+      final twoMonthsAgo = DateTime(now.year, now.month - 2, 1);
+
+      test(
+        "Given an oldest date should load all the months that have data",
+        () async {
+          final sut = StatsBloc(mockrepo);
+          when(
+            mockrepo.getOldestExpenseDate(),
+          ).thenAnswer((_) async => twoMonthsAgo);
+
+          await sut.loadAvailableMonths();
+
+          expect(sut.availableMonths, [currentMonth, lastMonth, twoMonthsAgo]);
+        },
+      );
+
+      test("Given no oldest date should load only the current month", () async {
+        final sut = StatsBloc(mockrepo);
+        when(mockrepo.getOldestExpenseDate()).thenAnswer((_) async => null);
+
+        await sut.loadAvailableMonths();
+
+        expect(sut.availableMonths, [currentMonth]);
+      });
+
+      test(
+        "Given the current month as oldest date should load only the current month",
+        () async {
+          final sut = StatsBloc(mockrepo);
+          when(
+            mockrepo.getOldestExpenseDate(),
+          ).thenAnswer((_) async => currentMonth);
+
+          await sut.loadAvailableMonths();
+
+          expect(sut.availableMonths, [currentMonth]);
+        },
+      );
     });
 
     test("Should load no categories as default value", () {
-      final sut = StatsBloc();
+      final sut = StatsBloc(mockrepo);
 
       expect(sut.selectedCategories, isEmpty);
     });
 
     test("Given a new category list then it should update the state", () {
-      final sut = StatsBloc();
+      final sut = StatsBloc(mockrepo);
       var expected = [ExpenseCategory.food, ExpenseCategory.travel];
 
       sut.onCategoriesUpdated(expected);
 
       expect(sut.selectedCategories, expected);
-
     });
   });
 }
