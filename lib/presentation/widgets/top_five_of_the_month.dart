229d1160034000343204110b8c1bead865440b0f
David Benitez <dbr.bcn@gmail.com>
2025-09-02 13:36:55 +0200

    adds Top 5 this month widget

--- lib/presentation/widgets/top_five_of_the_month.dart
+++ lib/presentation/widgets/top_five_of_the_month.dart
@@ -1,10 +1,13 @@
+import 'package:dukoin/domain/expense.dart';
 import 'package:dukoin/domain/state_status.dart';
 import 'package:dukoin/l10n/app_localizations.dart';
 import 'package:dukoin/presentation/state/stats_page_state.dart';
+import 'package:dukoin/presentation/widgets/currency_text.dart';
 import 'package:dukoin/presentation/widgets/dukoin_shimmer.dart';
-import 'package:dukoin/presentation/widgets/expense_info_card.dart';
 import 'package:dukoin/styles/dukoin_colors.dart';
+import 'package:dukoin/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class TopFiveOfTheMonth extends StatelessWidget {
@@ -53,14 +56,87 @@ class TopFiveOfTheMonth extends StatelessWidget {
                     ],
                   ),
                 ),
-                ...statsBloc.topFiveExpenses.map(
-                  (e) => ExpenseInfoCard(expense: e),
-                ),
+                TopExpensesListCard(expenses: statsBloc.topFiveExpenses),
               ],
             );
           },
         );
       },
+    );
+  }
+}
+
+class TopExpensesListCard extends StatelessWidget {
+  final List<Expense> expenses;
+
+  const TopExpensesListCard({super.key, required this.expenses});
+
+  Widget _buildExpenseItem(BuildContext context, Expense expense, int index) {
+    Color golden = Theme.of(context).extension<DukoinColors>()!.goldenEra;
+    Widget numberCircle = CircleAvatar(
+      backgroundColor: golden.withAlpha(64),
+      child: Text(
+        "#${index + 1}",
+        style: TextStyle(color: golden, fontWeight: FontWeight.w600),
+      ),
+    );
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            children: [
+              numberCircle,
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            expense.description,
+                            style: TextTheme.of(context).displaySmall,
+                            softWrap: true, // allows multiple lines
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "${getCategoryTitle(context, expense.category)} ${getIconFromCategory(expense.category)} · ${DateFormat('MMM d', AppLocalizations.of(context)!.localeName).format(expense.createdAt)}",
+                      style: TextTheme.of(context).bodyMedium,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(width: 16),
+              CurrencyText(
+                expense.amount,
+                style: TextTheme.of(context).displaySmall,
+              ),
+            ],
+          ),
+        ),
+        if (index < expenses.length - 1) Divider(height: 24),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card.outlined(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
+          children: List.generate(
+            expenses.length,
+            (i) => _buildExpenseItem(context, expenses[i], i),
+          ),
+        ),
+      ),
     );
   }
 }
