fb4c5cf0d4793b12fbcacb2cfad9b5b1b01484bf
David Benitez <dbr.bcn@gmail.com>
2025-09-03 23:15:50 +0200

    add animated constructor

--- lib/presentation/widgets/currency_text.dart
+++ lib/presentation/widgets/currency_text.dart
@@ -6,15 +6,41 @@ import 'package:flutter/material.dart';
 class CurrencyText extends StatelessWidget {
   final double amount;
   final TextStyle? style;
+  final bool isAnimated;
+  final int durationInMilliseconds;
 
-  const CurrencyText(this.amount, {super.key, this.style});
+  const CurrencyText(this.amount, {super.key, this.style})
+    : isAnimated = false,
+      durationInMilliseconds = 0;
+
+  const CurrencyText.animated(
+    this.amount, {
+    super.key,
+    this.durationInMilliseconds = 250,
+    this.style,
+  }) : isAnimated = true;
 
   @override
   Widget build(BuildContext context) {
+    return isAnimated ? _buildAnimated() : _buildText(context, amount);
+  }
+
+  TweenAnimationBuilder<double> _buildAnimated() {
+    return TweenAnimationBuilder<double>(
+      tween: Tween<double>(begin: amount, end: amount),
+      duration: Duration(milliseconds: durationInMilliseconds),
+      curve: Curves.easeInCubic,
+      builder: (context, value, child) {
+        return _buildText(context, value);
+      },
+    );
+  }
+
+  Text _buildText(BuildContext context, double value) {
     return Text(
       formatCurrency(
         CurrencyProvider.of(context).currency,
-        amount,
+        value,
         AppLocalizations.of(context)!.localeName,
       ),
       style:
