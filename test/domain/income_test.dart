c4c9869a219547b626ee4bcdf4101ea17e1478dc
David Benitez <dbr.bcn@gmail.com>
2025-09-12 21:16:15 +0200

    adds income class

---
+++ test/domain/income_test.dart
@@ -0,0 +1,50 @@
+import 'package:dukoin/domain/income.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group("Income", () {
+    final now = DateTime.now();
+    group("fromMap", () {
+      test("It should create an Expense from a map", () {
+        Map<String, dynamic> map = {
+          "id": 1,
+          "amount": 2.3,
+          "category": IncomeCategory.salary.name,
+          "description": "Title",
+          "createdAt": now.toIso8601String(),
+        };
+
+        Income sut = Income.fromMap(map);
+
+        expect(sut.id, 1);
+        expect(sut.description, "Title");
+        expect(sut.amount, 2.3);
+        expect(sut.category, IncomeCategory.salary);
+        expect(sut.isExpense, false);
+        expect(sut.createdAt, DateTime(now.year, now.month, now.day));
+      });
+    });
+    group("toMap", () {
+      test("It should create a map from an Income", () {
+        Map<String, dynamic> expected = {
+          "id": 1,
+          "amount": 2.3,
+          "category": IncomeCategory.salary.name,
+          "description": "Title",
+          "isExpense": 0,
+          "createdAt": DateTime(now.year, now.month, now.day).toIso8601String(),
+        };
+
+        Income sut = Income(
+          id: 1,
+          amount: 2.3,
+          category: IncomeCategory.salary,
+          description: "Title",
+          createdAt: now,
+        );
+
+        expect(sut.toMap(), expected);
+      });
+    });
+  });
+}
